47f5820ca81cf37593813b211f8be047cbf4151e
Yeo Yek Chong <ycyeo@yahoo.com>
2016-08-20 01:35:20 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer( function(request, response) {
+   console.log('Got Request Headers: ');
+   console.log(request.headers);
+   response.writeHead(200, {}
+      'Content-Type': 'text/plain'
+   );
+   response.write('PONG');
+   response.end();
+}).listen(8080);
+
